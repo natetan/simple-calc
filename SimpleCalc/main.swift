b03e85e94efd80d441287f2f55c6c485f1b71f60
Yulong Tan <yulongtan67@gmail.com>
2016-10-06 16:51:46 -0700

    added other functions

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -10,6 +10,9 @@ import Foundation
 
 print("Enter an expression seperated by returns: ");
 let response1 = readLine(strippingNewline: true)!
+let line1 = String.init(response1);
+let arr : [String] = line1!.components(separatedBy: " ")
+
 let num1 = Int.init(response1);
 let response2 = readLine(strippingNewline: true)!
 let op = String.init(response2);
@@ -36,16 +39,31 @@ func mod(a: Int, b: Int) -> Int{
     return a % b;
 }
 
-func count() {
-    
+func count(numbers: [Int]) -> Int {
+    return numbers.count;
 }
 
-func avg() {
-    
+func avg(numbers: [Int]) -> Int {
+    var sum = 0;
+    var count = numbers.count;
+    var index = 0;
+    while (count > 0) {
+        sum += numbers[index];
+        index += 1;
+        count -= 1;
+    }
+    return sum / numbers.count;
 }
 
-func fact() {
-    
+func fact(num: Int) -> Int {
+    if (num < 0) {
+        print("You don't have any understanding of factorials if you put in a negative number")
+        return -1
+    } else if (num == 0 || num == 1) {
+        return 1;
+    } else {
+        return num * fact(num: (num - 1));
+    }
 }
 
 var result = 0;
@@ -63,14 +81,14 @@ if (op == "+") {
 } else if (op == "%") {
     result = mod(a: num1!, b: num2!);
 } else {
-	isValid = false;
-	print("Invalid operator: \(op)");
+    isValid = false;
+    print("Invalid operator: \(op)");
 }
 
 message = "Result: \(result)";
 
 if (isValid) {
-	print(message);
+    print(message);
 }
 
 
